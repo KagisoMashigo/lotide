d086670a023ba4d019ba0c378be6aee4e93ba229
Iso <kagiso.k.mashigo@gmial.com>
2020-09-14 20:29:29 +0000

    Assert Equal function code

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("🕺🕺🕺 Assertion Passed: " + actual + " === " + expected + "");
+  } else if (actual !== expected) {
+    console.log("🤦🤦🤦 Assertion Failed: " + actual + " !== " + expected + "");
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual(2, 1);
