4c2c1b8dcd2fbb58c484e3fd0ebbd364061926e3
Iso <kagiso.k.mashigo@gmial.com>
2020-09-19 17:13:22 +0000

    letterPositions object function complete and working

--- letterPositions.js
+++ letterPositions.js
@@ -19,29 +19,20 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 
-// this function needs to take in a string
-const countLetters = function(string) {
-  let counter = {};
-  for (let char of string) {
-    if (char !== " ") {// it must ignore spaces " "
-    if (counter[char]) { //if this character is already a key in object the next code block adds +1 to the value
-      counter[char] += 1; 
-    } else {
-      counter[char] = 1;
+const letterPositions = function(sentence) {
+    const results = {};
+    const lowerCase = sentence.toLowerCase();
+    for (let i = 0; i < lowerCase.length; i++) {
+      if (lowerCase[i] !== " ") {
+        if (results[lowerCase[i]]) {
+          results[lowerCase[i]].push(i);
+        } else {
+          results[lowerCase[i]] = [i];
+        }
+      }
     }
-   }
-  } return counter;// it must then return the object
-}
+    return results;
+  };
 
-assertArraysEqual(countLetters("lighthouse in the house"), {
-  l: 1,
-  i: 2,
-  g: 1,
-  h: 4,
-  t: 2,
-  o: 2,
-  u: 2,
-  s: 2,
-  e: 3,
-  n: 1,
-});
+assertArraysEqual(letterPositions("hello").e, [1]);
+assertArraysEqual(letterPositions("hello"). l, [2, 3]);
