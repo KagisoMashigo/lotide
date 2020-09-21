17a086023182ce60b61ea6ce519fbb764b947504
Iso <kagiso.k.mashigo@gmial.com>
2020-09-21 19:41:55 +0000

    added M&C tests for headTest

--- middle.js
+++ middle.js
@@ -1,45 +1,27 @@
-const eqArrays = function(actual, expected) {
-  if (actual.length !== expected.length) {
-    return false;
-  } else {
-    for (let i in actual) {
-      if (actual[i] !== expected[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-};
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`🕺🕺🕺 Assertion Passed: ${JSON.stringify(actual)} === ${JSON.stringify(expected)}`);
-  } else if (!eqArrays(actual, expected)) {
-    console.log(`🤦🤦🤦 Assertion Failed: ${JSON.stringify(actual)} !== ${JSON.stringify(expected)}`);
-  }
-};
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
 
 const middle = function(arr) {
-  console.log('input array:', arr);
+  //console.log('input array:', arr);
   // We assume that array length is at most 2
   let startIndex = 1;
   let endIndex = 0;
   if (arr.length > 2) {
-    console.log('array length is more than 2');
+    //console.log('array length is more than 2');
     if (arr.length % 2 === 1) {
       // Array length is odd
-      console.log('array length is an odd number');
+      //console.log('array length is an odd number');
       startIndex = Math.floor(arr.length / 2);
       endIndex = Math.floor(arr.length / 2) + 1; // Could also be Math.ceil(arr.length / 2)
-      console.log('startIndex:', startIndex, 'endIndex:', endIndex);
+     // console.log('startIndex:', startIndex, 'endIndex:', endIndex);
     } else {
-      console.log('array length is an even number');
+     // console.log('array length is an even number');
       // Assume length is even
       startIndex = arr.length / 2 - 1;
       endIndex = arr.length / 2 + 1;
     }
-  } else {// more debug info, not necessary
-    console.log('array length is not more than 2:', arr.length);
+//  } else {// more debug info, not necessary
+    //console.log('array length is not more than 2:', arr.length);
   }
 
   const result = arr.slice(startIndex, endIndex);
@@ -47,15 +29,4 @@ const middle = function(arr) {
   return result;
 };
 
-
-
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); //=> 3
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);// => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3,4]); // => [3, 4]
-
-//middle needs to take in an array and count the number of elements
-// if even it must extract the two middle values
-// if odd it must take the single middle value
+module.exports = middle;
