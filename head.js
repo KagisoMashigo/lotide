eeae056a1000ff9c1e8a0df9d5585340412e7682
Iso <kagiso.k.mashigo@gmial.com>
2020-09-21 13:26:29 +0000

    head.js linted

--- head.js
+++ head.js
@@ -1,20 +1,21 @@
 const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-      console.log(`🕺🕺🕺 Assertion Passed: ${actual} === ${expected}`);
-    } else if (actual !== expected) {
-      console.log(`🤦🤦🤦 Assertion Failed: ${actual} !== ${expected}`);
-    }
-  };
+  if (actual === expected) {
+    console.log(`🕺🕺🕺 Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🤦🤦🤦 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
   
-  // TEST CODE
-  assertEqual("Lighthouse Labs", "Bootcamp");
-  assertEqual(1, 1);
-  assertEqual("Bootcamp", "Bootcamp");
-  assertEqual(2, 1);
+const head = function(array) {
+  return array[0];
+};
 
-function head (array){
-    return array[0];
-  };
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual(2, 1);
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 
-  assertEqual(head([5,6,7]), 5);
-  assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
