b996d208b7c7e38fea3249648f1e3e61e8a4d644
Iso <kagiso.k.mashigo@gmial.com>
2020-09-21 17:31:47 +0000

    eqArrays refractored and eqTest added

--- eqArrays.js
+++ eqArrays.js
@@ -1,13 +1,9 @@
-const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-      console.log(`🕺🕺🕺 Assertion Passed: ${actual} === ${expected}`);
-    } else if (actual !== expected) {
-      console.log(`🤦🤦🤦 Assertion Failed: ${actual} !== ${expected}`);
-    }
-  };
+const assertEqual = require('./assertEqual');
+
 // the reason it didn't work at first is bc two different arrays (whilst having the same values)
 // are not the same, they are two unique arrays with the same values
 // thus the need to check each value inside then arises
+
 const eqArrays = function(a, b){
     let isEqual = true;
     for ( let i in b){  //its going through the arrays and checking if each value is ===
@@ -18,10 +14,7 @@ const eqArrays = function(a, b){
   return isEqual
 };
 
+module.exports = eqArrays;
+
 // we're checking to see if the valuels of the below arrays are =//
-
-
 // we're creating a loop to loop through both arrays and see if they return the same vaules//
-
-assertEqual(eqArrays([1, 4, 5], [1, 4, 5]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
---
+++ test/eqArraysTest.js
@@ -0,0 +1,5 @@
+const assertEqual = require('../assertEqual');
+const eqArrays = require('../eqArrays');
+
+assertEqual(eqArrays([1, 4, 5], [1, 4, 5]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
