860ed794743642dae59d0dfb7cc841e3378f16bd
Iso <kagiso.k.mashigo@gmial.com>
2020-10-26 16:09:56 +0000

    gitignore file added

--- findKey.js
+++ findKey.js
@@ -11,7 +11,7 @@ const movies = {
 
 const findKey = function(object, callBack) {
   for (let key in object){
-    if (callBack(object[key])) {
+    if (callBack(object[key])) { //example of how to invoke a function wihtin a function
       return key;
     } 
   }
