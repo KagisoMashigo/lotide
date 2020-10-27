4fc36789d06959f051a9f3e4454324fb604308c5
Iso <kagiso.k.mashigo@gmial.com>
2020-10-27 22:25:22 +0000

    middle cleaned up

--- middle.js
+++ middle.js
@@ -2,31 +2,25 @@ const eqArrays = require('./eqArrays');
 const assertArraysEqual = require('./assertArraysEqual');
 
 const middle = function(arr) {
-  //console.log('input array:', arr);
-  // We assume that array length is at most 2
   let startIndex = 1;
   let endIndex = 0;
   if (arr.length > 2) {
-    //console.log('array length is more than 2');
     if (arr.length % 2 === 1) {
-      // Array length is odd
-      //console.log('array length is an odd number');
       startIndex = Math.floor(arr.length / 2);
-      endIndex = Math.floor(arr.length / 2) + 1; // Could also be Math.ceil(arr.length / 2)
-     // console.log('startIndex:', startIndex, 'endIndex:', endIndex);
+      endIndex = Math.floor(arr.length / 2) + 1; 
     } else {
-     // console.log('array length is an even number');
-      // Assume length is even
       startIndex = arr.length / 2 - 1;
       endIndex = arr.length / 2 + 1;
     }
-//  } else {// more debug info, not necessary
-    //console.log('array length is not more than 2:', arr.length);
   }
 
   const result = arr.slice(startIndex, endIndex);
-  // console.log('result for', arr, 'is:', result);
   return result;
 };
+
+assertArraysEqual(middle([1, 2, 3]), [2]) 
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]) 
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3])
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4])
 
 module.exports = middle;
